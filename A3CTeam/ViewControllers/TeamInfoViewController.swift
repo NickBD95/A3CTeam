54ffdc2570fa166b148048107c706663a7818e37
deshollow <555demin@gmail.com>
2023-11-04 13:47:35 +0300

    Team info ViewController code in progress

---
+++ A3CTeam/ViewControllers/TeamInfoViewController.swift
@@ -0,0 +1,44 @@
+//
+//  TeamInfoViewController.swift
+//  A3CTeam
+//
+//  Created by deshollow on 04.11.2023.
+//
+
+import UIKit
+
+class TeamInfoViewController: UIViewController {
+    
+    @IBOutlet weak var achivementTableView: UITableView!
+    
+    private let achivements = Achivement.getAchivement()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        achivementTableView.delegate = self
+        achivementTableView.dataSource = self
+    }
+}
+
+extension TeamInfoViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //print("you tapped me!")
+    }
+}
+
+extension TeamInfoViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        4
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+}
+
+    
